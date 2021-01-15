4121e658dee59d5b5ea8723d57ec44b2fe162e5b
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-15 23:25:07 +0530

    Party Playlist

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['Sage', 'Tommy', 'Kya Kruu', 'Rasode', 'Magnetta Riddim', 'Taki']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
